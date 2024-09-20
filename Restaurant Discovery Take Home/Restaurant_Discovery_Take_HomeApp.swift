3204bada17875be3a0bf951c7a343831bd61eddd
DiNerdyYawdie <62502925+DiNerdyYawdie@users.noreply.github.com>
2024-09-20 10:46:08 -0400

    Initial Commit

---
+++ Restaurant Discovery Take Home/Restaurant_Discovery_Take_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_Discovery_Take_HomeApp.swift
+//  Restaurant Discovery Take Home
+//
+//  Created by Chad-Michael Muirhead on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_Discovery_Take_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
